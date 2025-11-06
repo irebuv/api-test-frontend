c59742cc26c643031aac7832344fd509665dead7
irebuv <irebuv671@gmail.com>
2025-11-06 16:36:17 +0200

    rate limiter

--- src/layouts/app-content.tsx
+++ src/layouts/app-content.tsx
@@ -1,25 +1,9 @@
-import {PropsWithChildren, useRef} from 'react';
-import {toast, Toaster} from "@/components/ui/custom/sonner";
-import {useLocation, useNavigate} from "react-router-dom";
-import {useEffect} from "react";
+import {PropsWithChildren} from 'react';
 
 export default function MainContent({children} : PropsWithChildren){
-    const location = useLocation();
-    const navigate = useNavigate();
-    const toastShownRef = useRef(false);
-
-    useEffect(() => {
-        if (location.state?.toastMessage && !toastShownRef.current) {
-            toastShownRef.current = true;
-            toast.error(location.state.toastMessage);
-
-            navigate(location.pathname, { replace: true, state: {} });
-        }
-    }, [location, navigate]);
 
     return (
         <>
-            <Toaster />
             {children}
         </>
     );
